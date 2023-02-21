80a42a0e1584af2127867c747b4378ba20a71d6d
doquockhanhan <doquockhanhan@gmail.com>
2023-02-21 14:36:23 +0700

    update build step

--- ci/build.sh
+++ ci/build.sh
@@ -3,5 +3,6 @@ set -xe
 
 #Login to registry
 echo $GITHUB_PASSWORD | docker login ghcr.io -u $GITHUB_USERNAME --password-stdin
-#Push image
-docker-compose push
+#Build and push image
+docker build -t ${CONTAINER_RELEASE_IMAGE} -f Dockerfile .
+docker push ${CONTAINER_RELEASE_IMAGE}
